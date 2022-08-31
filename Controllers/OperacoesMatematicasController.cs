5a7666c405a2c693023fd0f1e5dbdde7deb4398b
weronicamelo <101827890+weronicamelo@users.noreply.github.com>
2022-08-31 09:06:04 -0300

    Add files via upload

---
+++ Controllers/OperacoesMatematicasController.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcIntroViews.Controllers;
+
+public class OperacoesMatematicasController : Controller
+{
+    public IActionResult Somar()
+    {
+        return View();
+    }
+
+    public IActionResult Subtrair()
+    {
+        return View();
+    }
+
+    public IActionResult Multiplicar()
+    {
+        return View();
+    }
+
+    public IActionResult ResultadoSoma([FromForm] double n1, [FromForm] double n2)
+    {
+        ViewBag.N1 = n1;
+        ViewBag.N2 = n2;
+        ViewBag.Resultado = n1 + n2;
+
+        return View();
+    }
+
+    public IActionResult ResultadoSubtracao([FromForm] double n1, [FromForm] double n2)
+    {
+        ViewBag.N1 = n1;
+        ViewBag.N2 = n2;
+        ViewBag.Resultado = n1 - n2;
+
+        return View();
+    }
+
+    public IActionResult ResultadoMultiplicacao([FromForm] double n1, [FromForm] double n2)
+    {
+        ViewBag.N1 = n1;
+        ViewBag.N2 = n2;
+        ViewBag.Resultado = n1 * n2;
+
+        return View();
+    }
+}
